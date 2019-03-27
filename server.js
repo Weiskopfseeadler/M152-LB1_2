fdd59a0ce10a7302a3b09f5f0868fd2ef8f84d8f
weiskopfseeadler <patrick.grunder@csbe.ch>
2019-03-27 20:24:10 +0100

    Better

--- server.js
+++ server.js
@@ -18,14 +18,51 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage });
 app.use("/files", express.static(__dirname + "/files"));
 // @ts-ignore
+// app.post('/file ', upload.single(''), function (req, res,next) {
+//
+//     // @ts-ignore
+//     gm('not_processed_files/'+req.file.filename).resize(720, null).noProfile().write(__dirname+"/files/"+"sm_"+req.file.originalname,
+//              function (err: any) {if (!err) console.log('done');else console.log("error");})
+//     // @ts-ignore
+//     gm('not_processed_files/'+req.file.filename).resize(1280, null).noProfile().write(__dirname+"/files/"+"med_"+req.file.originalname,
+//         function (err: any) {if (!err) console.log('done');else console.log("error");});
+//     // @ts-ignore
+//     gm('not_processed_files/'+req.file.filename).resize(1920, null).noProfile().write(__dirname+"/files/"+"big_"+req.file.originalname,
+//         function (err: any) {if (!err) console.log('done');else console.log("error");});
+//     // @ts-ignore
+//     gm('not_processed_files/'+req.file.filename).noProfile().write(__dirname+"/files/"+"ori_"+req.file.originalname,
+//         function (err: any) {if (!err) console.log('done');else console.log("error");});res.status(200).send("ok");
+// });
+// @ts-ignore
+app.use("/files", express.static(__dirname + "/files"));
+// @ts-ignore
 app.post('/file', upload.single(''), function (req, res, next) {
     // @ts-ignore
-    gm('not_processed_files/' + req.file.filename).resize(240, 240).noProfile().write(__dirname + "/files/" + "sm_" + req.file.originalname, function (err) {
+    gm('not_processed_files/' + req.file.filename).resize(720, null).noProfile().write(__dirname + "/files/" + "sm_" + req.file.originalname, function (err) {
         if (!err)
             console.log('done');
         else
             console.log(err);
     });
+    gm('not_processed_files/' + req.file.filename).resize(1200, null).noProfile().write(__dirname + "/files/" + "med_" + req.file.originalname, function (err) {
+        if (!err)
+            console.log('done');
+        else
+            console.log(err);
+    });
+    gm('not_processed_files/' + req.file.filename).resize(1920, null).noProfile().write(__dirname + "/files/" + "big_" + req.file.originalname, function (err) {
+        if (!err)
+            console.log('done');
+        else
+            console.log(err);
+    });
+    gm('not_processed_files/' + req.file.filename).resize(null, null).noProfile().write(__dirname + "/files/" + "ori_" + req.file.originalname, function (err) {
+        if (!err)
+            console.log('done');
+        else
+            console.log(err);
+    });
+    res.sendFile(__dirname + "/index.html");
     res.status(200).send("ok");
 });
 app.get('/', function (req, res) {
